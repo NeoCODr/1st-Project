1e8ce84484e9d20a88c07f7205a487ca9a93b3db
Anmol Semalty <anmolsemalty07@gmail.com>
2025-07-09 06:10:30 +0530

    Battery Pop Up Notifier
    Add battery overcharging notifier script
    
    This script sends a desktop notification when the battery level reaches 99%.
    It helps prevent overcharging and improves long-term battery health.

---
+++ Battery Notifier.py
@@ -0,0 +1,35 @@
+from plyer import notification
+import time
+import batteryinfo as bi
+from playsound3 import playsound
+
+
+prev_percent = None
+notified = False
+
+while True:
+    Batter = bi.Battery()
+    Battery = round(Batter.percent.value)
+    Battery2 = round(Batter.percent.value, 2)
+    State = Batter.state
+
+    if prev_percent != None:
+        Discharge = round(Battery2 - prev_percent, 2)
+        print(Discharge)
+    prev_percent = Battery2
+
+    if Battery >= 99 and State.lower() == "charging" and notified == False:
+        notified = True
+        
+
+        notification.notify(
+            title='Overcharging Alert',
+            message='Your Device Has Charged, Please Disconnect The Power Plug',
+            timeout=9,
+        )
+        playsound("C:/Users/ankit/OneDrive/Desktop/python/SCRAP YARD/beep.mp3")
+
+    if Battery < 99 and State.lower() != "charging":
+        notified = False
+
+    time.sleep(60)
